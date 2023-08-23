037f0fedc7907442979c25c3f8c86e3b96898b8d
Yuhao Wang <52476526+wyhwhy@users.noreply.github.com>
2023-08-23 22:45:43 +0800

    Update douban.py

--- Douban_code/dataset/douban.py
+++ Douban_code/dataset/douban.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 import torch.utils.data
 
-
-class Douban(torch.utils.data.Dataset):
+class Douban(torch.utils.data):
     """
     Douban Dataset
 
@@ -14,33 +13,20 @@ class Douban(torch.utils.data.Dataset):
         https://github.com/FengZhu-Joey/GA-DTCDR/tree/main/Data
     """
 
-    def __init__(self, dataset_path, sep='\t', engine='c', header=None):
-        
-        dataset_path1 = dataset_path + 'douban_music/ratings.dat'
-        dataset_path2 = dataset_path + 'douban_book/ratings.dat'
-        dataset_path3 = dataset_path + 'douban_movie/ratings.dat'
-        
-        data1 = pd.read_csv(dataset_path1,sep='\t',header=None).to_numpy()[:, :3]
-        data2 = pd.read_csv(dataset_path2,sep='\t',header=None).to_numpy()[:, :3]
-        data3 = pd.read_csv(dataset_path3,sep='\t',header=None).to_numpy()[:, :3]
-        
-        self.items1 = data1[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
-        self.items2 = data2[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
-        self.items3 = data3[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
-        self.items2[:,1] = self.items2[:,1] + 5567
-        self.items3[:,1] = self.items3[:,1] + 5567 + 6777
-        self.items = np.concatenate((self.items1, self.items2, self.items3), axis=0)
-        
-        self.targets1 = self.__preprocess_target(data1[:, 2]).astype(np.int)
-        self.targets2 = self.__preprocess_target(data2[:, 2]).astype(np.int)
-        self.targets3 = self.__preprocess_target(data3[:, 2]).astype(np.int)
-        self.targets = np.concatenate((self.targets1, self.targets2, self.targets3), axis=0)
-        
+    def __init__(self, mode, path_base='/Douban/dataset/'):
+        #mode: train / val / test
+        dataset_name = ['douban_music', 'douban_book', 'douban_movie']
+        path = path_base + dataset_name[0] + '_' + mode + '.csv'
+        data = pd.read_csv(path).to_numpy()[:, :4]
+        for i in range(1, len(dataset_name)):
+            path = path_base + dataset_name[i] + '_' + mode + '.csv'
+            data_on = pd.read_csv(path).to_numpy()[:, :4]
+            data = np.concatenate((data, data_on), 0)
+        self.items = data[:, :3].astype(np.int)
+        self.targets = data[:, 3].astype(np.float32)
         self.field_dims = np.ndarray(shape=(2,), dtype=int)
         self.field_dims[0] = 2718
-        self.field_dims[1] = 5567 + 6777 + 9565
-        self.user_field_idx = np.array((0, ), dtype=np.long)
-        self.item_field_idx = np.array((1,), dtype=np.long)
+        self.field_dims[1] = 21909 # music 5567 + book 6777 + movie 9565
 
     def __len__(self):
         return self.targets.shape[0]
@@ -48,27 +34,19 @@ class Douban(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return self.items[index], self.targets[index]
 
-    def __preprocess_target(self, target):
-        target[target <= 3] = 0
-        target[target > 3] = 1
-        return target
 
+class DoubanMusic(torch.utils.data):
 
-class DoubanMusic(torch.utils.data.Dataset):
-
-    def __init__(self, dataset_path, sep='\t', engine='c', header=None):
-        data = pd.read_csv(dataset_path,sep=sep,header=header).to_numpy()[:, :3]
-        self.items = data[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
-        self.targets = self.__preprocess_target(data[:, 2]).astype(np.int)
+    def __init__(self, mode, path_base='/Douban/dataset/'):
+        #mode: train / val / test
+        dataset_name = ['douban_music']
+        path = path_base + dataset_name[0] + '_' + mode + '.csv'
+        data = pd.read_csv(path).to_numpy()[:, :4]
+        self.items = data[:, :3].astype(np.int)
+        self.targets = data[:, 3].astype(np.float32)
         self.field_dims = np.ndarray(shape=(2,), dtype=int)
-        #self.field_dims[0] = np.max(self.items[:,0])+1
-        #self.field_dims[1] = np.max(self.items[:,1])+1
         self.field_dims[0] = 2718
         self.field_dims[1] = 5567 + 6777 + 9565
-        #print(self.field_dims)
-        self.user_field_idx = np.array((0, ), dtype=np.long)
-        self.item_field_idx = np.array((1,), dtype=np.long)
-        self.items[:,1] = self.items[:,1] + 0
 
     def __len__(self):
         return self.targets.shape[0]
@@ -76,56 +54,18 @@ class DoubanMusic(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return self.items[index], self.targets[index]
 
-    def __preprocess_target(self, target):
-        target[target <= 3] = 0
-        target[target > 3] = 1
-        return target
+class DoubanBook(torch.utils.data):
 
-class DoubanBook(torch.utils.data.Dataset):
-
-    def __init__(self, dataset_path, sep='\t', engine='c', header=None):
-        data = pd.read_csv(dataset_path,sep=sep,header=header).to_numpy()[:, :3]
-        self.items = data[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
-        self.targets = self.__preprocess_target(data[:, 2]).astype(np.int)
+    def __init__(self, mode, path_base='/Douban/dataset/'):
+        #mode: train / val / test
+        dataset_name = ['douban_book']
+        path = path_base + dataset_name[0] + '_' + mode + '.csv'
+        data = pd.read_csv(path).to_numpy()[:, :4]
+        self.items = data[:, :3].astype(np.int)
+        self.targets = data[:, 3].astype(np.float32)
         self.field_dims = np.ndarray(shape=(2,), dtype=int)
-        #self.field_dims[0] = np.max(self.items[:,0])+1
-        #self.field_dims[1] = np.max(self.items[:,1])+1
         self.field_dims[0] = 2718
         self.field_dims[1] = 5567 + 6777 + 9565
-        #print(self.field_dims)
-        self.user_field_idx = np.array((0, ), dtype=np.long)
-        self.item_field_idx = np.array((1,), dtype=np.long)
-        self.items[:,1] = self.items[:,1] + 5567
-        
-    def __len__(self):
-        return self.targets.shape[0]
-
-    def __getitem__(self, index):
-        return self.items[index], self.targets[index]
-
-    def __preprocess_target(self, target):
-        target[target <= 3] = 0
-        target[target > 3] = 1
-        return target
-
-class DoubanMovie(torch.utils.data.Dataset):
-
-    def __init__(self, dataset_path, sep='\t', engine='c', header=None):
-        data = pd.read_csv(dataset_path,sep=sep,header=header).to_numpy()[:, :3]
-        self.items = data[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
-        
-        self.targets = self.__preprocess_target(data[:, 2]).astype(np.int)
-        self.field_dims = np.ndarray(shape=(2,), dtype=int)
-        #self.field_dims[0] = 2712
-        #self.field_dims[1] = 34893
-        #self.field_dims[0] = np.max(self.items[:,0])+1
-        #self.field_dims[1] = np.max(self.items[:,1])+1
-        self.field_dims[0] = 2718
-        self.field_dims[1] = 5567 + 6777 + 9565
-        #print(self.field_dims)
-        self.user_field_idx = np.array((0, ), dtype=np.long)
-        self.item_field_idx = np.array((1,), dtype=np.long)
-        self.items[:,1] = self.items[:,1] + 5567 + 6777
 
     def __len__(self):
         return self.targets.shape[0]
@@ -133,7 +73,21 @@ class DoubanMovie(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return self.items[index], self.targets[index]
 
-    def __preprocess_target(self, target):
-        target[target <= 3] = 0
-        target[target > 3] = 1
-        return target
+class DoubanMovie(torch.utils.data):
+
+    def __init__(self, mode, path_base='/Douban/dataset/'):
+        #mode: train / val / test
+        dataset_name = ['douban_movie']
+        path = path_base + dataset_name[0] + '_' + mode + '.csv'
+        data = pd.read_csv(path).to_numpy()[:, :4]
+        self.items = data[:, :3].astype(np.int)
+        self.targets = data[:, 3].astype(np.float32)
+        self.field_dims = np.ndarray(shape=(2,), dtype=int)
+        self.field_dims[0] = 2718
+        self.field_dims[1] = 5567 + 6777 + 9565
+
+    def __len__(self):
+        return self.targets.shape[0]
+
+    def __getitem__(self, index):
+        return self.items[index], self.targets[index]
