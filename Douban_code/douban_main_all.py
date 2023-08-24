0b87dc4ec03fb98a18922e5ad5abfbc53c4ea6d6
Yuhao Wang <52476526+wyhwhy@users.noreply.github.com>
2023-08-24 13:01:47 +0800

    Update douban_main_all.py

--- Douban_code/douban_main_all.py
+++ Douban_code/douban_main_all.py
@@ -151,7 +151,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=2048)
     parser.add_argument('--weight_decay', type=float, default=1e-5)
     parser.add_argument('--device', default='cuda:0',help='cpu, cuda:0')
-    parser.add_argument('--save_dir', default='')
+    parser.add_argument('--save_dir', default='.')
     parser.add_argument('--job', type=int, default=1)
     args = parser.parse_args()
     main(args.dataset_name,
